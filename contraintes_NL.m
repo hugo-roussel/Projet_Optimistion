9fcdabb16d1adfdbb7df08ee8ee4f8ed22900023
hugo-roussel <52688792+hugo-roussel@users.noreply.github.com>
2019-11-27 16:27:20 +0100

    fct_etat

--- contraintes_NL.m
+++ contraintes_NL.m
@@ -1,6 +1,6 @@
 function [ceq,cineq]=contraintes_NL(x)
 % Contraintes relatives aux rampes
-global rampeDown24 rampeUp24 Demand
+global rampeDown24 rampeUp24 Demand p_min24 p_max24
 for k=1:12*24
     cineq(k)=x(k+1)-x(k)-rampeUp24(k);
     cineq(k+12*24)=x(k)-rampeDown24(k)-x(k+1);
@@ -15,18 +15,22 @@ for k=1:12
     cineq(k+12+576)=-minimumUpTime(k)+Temps2Fonctionnement(k+12);
 end
 
+% Contraintes relatives à l'état de fonct. des machines
+E=fct_etat(x);
+for k=1:12
+    for i=1:24
+    cineq(600+24*(k-1)+i)=x(24*(k-1)+i) - E(24*(k-1)+i)*p_max24(24*(k-1)+i);
+    cineq(600+288+24*(k-1)+i)=-(x(24*(k-1)+i) - E(24*(k-1)+i)*p_min24(24*(k-1)+i));
+    end
+end
+
 % Contraintes pour la satisfaction de la demande
 P = x(1:12*24); %puissances de chaque machine pour chaque heure
 for k=1:24
     ceq(k)=sum(P(k:24:12*24)) - Demand(k);
 end
-<<<<<<< HEAD
 
 end
-
-=======
-end
->>>>>>> 7aa5e3020721dc2e9705ed722d783f20cd4e13ad
 
 
 
